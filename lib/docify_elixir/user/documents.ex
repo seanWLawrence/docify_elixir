7ee807769851ba4ddb72725fa14bc894dcc69741
Sean Lawrence <sean.w.lawrence@gmail.com>
2019-03-07 09:26:06 -0600

    create user documents

---
+++ lib/docify_elixir/user/documents.ex
@@ -0,0 +1,18 @@
+defmodule DocifyElixir.User.Documents do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "documents" do
+    field :context, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(documents, attrs) do
+    documents
+    |> cast(attrs, [:context])
+    |> validate_required([:context])
+  end
+end
---
+++ priv/repo/migrations/20190307152442_create_documents.exs
@@ -0,0 +1,12 @@
+defmodule DocifyElixir.Repo.Migrations.CreateDocuments do
+  use Ecto.Migration
+
+  def change do
+    create table(:documents) do
+      add :context, :text
+
+      timestamps()
+    end
+
+  end
+end
